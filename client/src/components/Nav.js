6a8cba4b494ef74703c77cc4f83a988a03904b0b
dy27777 <yipdeyao@gmail.com>
2022-04-17 22:11:54 +0800

    basic server setup and frontend testing

---
+++ client/src/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <h1>Petitions</h1>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ client/src/components/Post.js
@@ -0,0 +1,15 @@
+const Post = () => {
+  return (
+    <div className="post">
+      <img
+        src="https://assets.nst.com.my/images/articles/dirisjrs_1647088111.jpg"
+        className="post_img"
+      />
+      <h3>Sam Ke Ting is INNOCENT!</h3>
+      <p>800,000 petition signed!</p>
+      <div className="post_bar"></div>
+    </div>
+  );
+};
+
+export default Post;
